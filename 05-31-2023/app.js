3732a92698a3cb466844452a49feeec734fb6d7e
Sai <sainathkorlakunta958@gmail.com>
2023-05-31 23:34:58 -0500

    05-31-2023

---
+++ 05-31-2023/app.js
@@ -0,0 +1,18 @@
+const obj = {
+  firstName: "Sai Nath",
+  lastName: "Korlakunta",
+  dob: "11/25/1997",
+  email: "sainathk1554@gmail.com",
+  address: "1048 Mckinney",
+  hobbies: ["playing cricket", "playing badminton", "playing table tennis"],
+  achievements: [
+    "Captain of the basketball winning team in intramurals",
+    "Played in JNTU table tennis zonals tournament",
+    "Won many cups in cricket",
+  ],
+  phone: "2392984287",
+  designation: "full stack java developer",
+  university: "WSU",
+};
+
+console.log(obj.hobbies.slice(-1)[0]);
